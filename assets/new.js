26ccba3c43a2d4fae66c33990169ba4d7b1c797f
Makwana Jigar <jigar84252@gmail.com>
2023-07-06 15:04:12 +0530

    first commit

---
+++ assets/new.js
@@ -0,0 +1,145 @@
+
+ //->->->->  DOM functions...Most imp ..
+
+
+                            //DOM targeting method....
+
+                            var element;
+                            element=document.getElementsByClassName("p3").innerText;
+                            console.log(element);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*function MJ(){
+    document.write("<div class ='hh'> Hello</div>");
+}
+
+MJ();
+document.write("<br>");
+
+MJ();
+document.write("<br>");
+
+MJ();
+document.write("<br>");
+
+MJ();
+document.write("<br>");
+*/
+
+
+/*var a=18;
+
+if(a <= 10){
+    document.write( "<div class='jj'>you are under age..</div>");
+}else{
+    document.write("<div class='jj'>you are over age..</div>");
+}
+
+
+
+ 
+var day=6;
+document.write("<div class='qq'>");
+switch(day){
+
+        case 1:
+        document.write("Today is Mon..");
+        break;
+
+        case 2:
+        document.write("Today is Tue..");
+        break;
+
+        case 3:
+        document.write("Today is Wed..");
+        break;
+
+        case 4:
+        document.write("Today is ThU..");
+        break;
+
+        case 5:
+        document.write("Today is Fri..");
+        break;
+
+        case 6:
+        document.write( "Today is Sat..");
+        break;
+
+        case 7:
+        document.write("Today is Sun..");
+        break;
+    
+        default:
+        document.write("Enter valid week day");
+        break;
+}
+document.write("</div>");
+
+
+
+
+var day=7;
+
+switch(day){
+
+        case 1:
+        document.write("<div class='pp'>Today is Mon..</div>");
+        break;
+
+        case 2:
+        document.write("<div class='pp'>Today is Tue..</div>");
+        break;
+
+        case 3:
+        document.write("<div class='pp'>Today is Wed..</div>");
+        break;
+
+        case 4:
+        document.write("<div class='pp'>Today is Thu..</div>");
+        break;
+
+        case 5:
+        document.write("<div class='pp'>Today is Fri..</div>");
+        break;
+
+        case 6:
+        document.write( "<div class='pp'>Today is Sat..</div>");
+        break;
+
+        case 7:
+        document.write("<div class='pp'>Today is Sun..</div>");
+        break;
+    
+        default:
+        document.write("<div class='pp'>Enter valid week day</div>");
+        break;
+}
+*/
+
+
